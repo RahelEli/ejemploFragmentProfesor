44a577b2e94b36436a2a3266c4f9dda2519e86fc
Elisheva <Elisheva>
2016-11-13 00:28:46 -0600

    Ejemplo profesor

---
+++ app/src/main/java/com/example/usuario/fragment/PrimerFragment.java
@@ -0,0 +1,29 @@
+package com.example.usuario.fragment;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by USUARIO on 11/11/2016.
+ */
+
+public class PrimerFragment extends Fragment {
+    public PrimerFragment (){}
+
+    public static PrimerFragment getInstance (){
+        PrimerFragment fragment = new PrimerFragment();
+        return  fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.primer_fragment_layout,container,false);
+        return view;
+    }
+}
